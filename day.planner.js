ff4737d032e1164cf29782ed5ea116ea9e382276
Cody Michaud <cody.r.michaud@gmail.com>
2021-01-25 12:11:23 -0800

    creating local storage

--- day.planner.js
+++ day.planner.js
@@ -28,4 +28,46 @@ for (const property in workDay) {
     let timeId = "#time" + counter;
     let presentHour = moment().hour();
     let timeString = $(timeId).text();
+    let timeNumber = hourNumberFromHourString(timeString);
+    if (timeNumber < presentHour) {
+        $(textEntry).addClass("past-hour");
+    } else if (timeNumber > presentHour) {
+        $(textEntry).addClass("future-hour");
+    } else {
+        $(textEntry).addClass("present-hour");
+    }
+    counter++;
 }
+
+$("button").click(function () {
+    value = $(this).siblings("textarea").val();
+    hourString = $(this).siblings("div").text();
+
+    saveSchedule(hourString, value);
+});
+//Function to pull hour number for past, future, and present classes
+function hourNumberFromHourString(hourString) {
+    switch (hourString) {
+        case "8 AM": return 8;
+        case "9 AM": return 9;
+        case "10 AM": return 10;
+        case "11 AM": return 11;
+        case "12 PM": return 12;
+        case "1 PM": return 13;
+        case "2 PM": return 14;
+        case "3 PM": return 15;
+        case "4 PM": return 16;
+        case "5 PM": return 17;
+    }
+};
+//Setting local storage
+function loadCorrectDataset() {
+    result = localStorage.getItem("workDay")
+    return (result ? result : workDay);
+};
+//Intitializing local storage
+function intitializeLocalStorage() {
+    localStorage.setItem("workday", JSON.stringify(workDay));
+};
+// saving to local storage
+
